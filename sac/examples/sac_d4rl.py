e9e4bf6a29cbf3f2ea90ebd86acfc0610bec63b9
Shideh Rezaeifar <shideh@google.com>
2020-11-18 13:53:26 +0100

    refactoring

--- sac/examples/sac_d4rl.py
+++ sac/examples/sac_d4rl.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
             variant["KL"] = True
 
         else:
-            exp_dir = '{}_{:2g}'.format(exp_dir, args.beta)
+            exp_dir = '{}_{0:.2g}'.format(exp_dir, args.beta)
 
     else:
         exp_dir = '{}/offline/{}_{}'.format(args.env, timestamp, args.seed)
